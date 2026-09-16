9c553b9068b680c2aedec32047158fa1e1779b39
rcreecy <rcreecy@icloud.com>
2026-09-16 09:59:08 -0700

    Use SDK graphics service linkage on macOS

--- src/services.hpp
+++ src/services.hpp
@@ -1,13 +1,11 @@
 #pragma once
 #include <mods/service.hpp>
 #include <mods/svc/config.h>
+#include <mods/svc/gfx.h>
 #include <mods/svc/log.h>
 #include <mods/svc/ui.h>
 
-typedef struct GfxService GfxService;
-
 extern ModContext* mod_ctx;
-extern const GfxService* svc_gfx;
 extern const ConfigService* svc_config;
 extern const UiService* svc_ui;
 extern const LogService* svc_log;
